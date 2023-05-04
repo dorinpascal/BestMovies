2a64a8bf2a77f8ae5a6554edd4bd994bbd83edc4
dorinpascal <pascald@mail.ru>
2023-05-04 13:30:29 +0200

    testing the get movie details endpoint

---
+++ BestMovies.Bff.Test/MoviesFunctionTest/GetMovieDetailsEndpointTest/ParameterTest.cs
@@ -0,0 +1,36 @@
+﻿using BestMovies.Bff.Functions;
+using BestMovies.Bff.Interface;
+using BestMovies.Bff.Test.Helpers;
+
+namespace BestMovies.Bff.Test.MoviesFunctionTest.GetMovieDetailsEndpointTest;
+
+public class ParameterTest
+{
+    private readonly IMovieService _movieService;
+    private readonly MockLogger<MovieFunctions> _logger;
+
+    public ParameterTest()
+    {
+        _movieService = Substitute.For<IMovieService>();
+        _logger = Substitute.For<MockLogger<MovieFunctions>>();
+    }
+
+
+    [Fact]
+    public async Task GetMovieDetails_PassedParameter_IsNotValid()
+    {
+        //Arrange
+       
+
+        var function = new MovieFunctions(_movieService);
+        var _request = new DefaultHttpRequest(new DefaultHttpContext());
+        // ACT
+        var response = await function.GetMovieDetails(_request,0,_logger);
+        var result = (ContentResult)response;
+
+        //Assert
+        Assert.Equal(400, result.StatusCode);
+        _logger.Received().Log(LogLevel.Information, Arg.Is<string>(s => s.Contains("Invalid value for the id. The value must be greater than 0")));
+    }
+
+}
--- BestMovies.Bff/Helpers/ExceptionHelpers.cs
+++ BestMovies.Bff/Helpers/ExceptionHelpers.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace BestMovies.Shared.Helpers;
+namespace BestMovies.Bff.Helpers;
 
 public static class ExceptionHelpers
 {
