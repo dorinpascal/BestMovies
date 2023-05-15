431aaff9d87a3680045a2bbc898b20c36c0e280e
dorinpascal <pascald@mail.ru>
2023-05-15 16:28:13 +0200

    .

---
+++ BestMovies.Api/Repo/IReviewRepository.cs
@@ -0,0 +1,9 @@
+﻿using BestMovies.Shared.Dtos.Review;
+using System.Threading.Tasks;
+
+namespace BestMovies.Api.Repo;
+
+public interface IReviewRepository
+{
+    Task CreateReview(string userId, ReviewDto reviewDto);
+}
---
+++ BestMovies.Api/Repo/Impl/ReviewRepository.cs
@@ -0,0 +1,31 @@
+﻿using BestMovies.Api.Persistence;
+using BestMovies.Api.Persistence.Entity;
+using BestMovies.Shared.Dtos.Review;
+using System;
+using System.Threading.Tasks;
+
+namespace BestMovies.Api.Repo.Impl;
+
+public class ReviewRepository : IReviewRepository
+{
+    private readonly BestMoviesDbContext _dbContext;
+
+    public ReviewRepository(BestMoviesDbContext _dbContext)
+    {
+        this._dbContext = _dbContext;
+    }
+
+    public async Task CreateReview(string UserId,ReviewDto reviewDto)
+    {
+        var review = new Review
+            {
+            UserId=UserId,
+            Rating=reviewDto.Rating,
+            Comment=reviewDto.Comment,
+        };
+       
+        await _dbContext.Reviews.AddAsync(review);
+        await _dbContext.SaveChangesAsync();
+        
+    }
+}
