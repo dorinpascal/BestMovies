90ecae9802f3d7e77802b7e84ee1f9b9dba0550a
GeraltWitcher1 <denis22@inbox.lv>
2023-05-17 21:24:39 +0200

    added savedmovies functions

---
+++ BestMovies.Api/Extensions/SavedMovieExtensions.cs
@@ -0,0 +1,13 @@
+﻿using BestMovies.Api.Persistence.Entity;
+using BestMovies.Shared.Dtos.Movies;
+
+namespace BestMovies.Api.Extensions;
+
+public static class SavedMovieExtensions
+{
+    public static SavedMovieDto ToDto(this SavedMovie savedMovie) =>
+        new(
+            MovieId: savedMovie.MovieId,
+            IsWatched: savedMovie.IsWatched
+        );
+}
